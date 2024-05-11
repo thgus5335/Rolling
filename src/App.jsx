f96ebf1c09d002954adc55de7e98ec56030cc5ac
정기환 <joelkh784@gmail.com>
2024-05-11 14:27:26 +0900

    ✨ Feat: tanstack-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './styles/global.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <RouterProvider router={router} />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <RouterProvider router={router} />
+      </div>
+    </QueryClientProvider>
   );
 }
 
